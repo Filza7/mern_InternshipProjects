41ae3b9aa45d33db94b8fd7e1864be44a222f05d
Filza7 <fylzafatima7@gmail.com>
2025-06-25 18:12:37 +0500

    Attempt to solve in O(n) time complexity

--- weekTwo/quizTwo/AttemptTwo.js
+++ weekTwo/quizTwo/AttemptTwo.js
@@ -1,15 +1,15 @@
 let arr = [2, 8, 4, 6];
 let size = arr.length;
 let t = 10;
-let found1 = -1;
-let found2 = -1;
+let found1=-1;
+let found2=-1;
 
 for (let i = 0; i < size; i++) {
     let extra = t - arr[i];
     let j = arr.findIndex((num, index) => num === extra && index !== i);
     if (j !== -1) {
-        found1 = i;
-        found2 = j;
+        found1=i;
+        found2=j;
         break; 
     }
 }
@@ -18,3 +18,4 @@ if (found1 !== -1 && found2 !== -1) {
 } else {
     console.log("No two numbers sum up to the target.");
 }
+console.log("  ")
